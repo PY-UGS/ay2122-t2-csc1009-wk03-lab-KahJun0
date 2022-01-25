19223a9ca7cd9dfc0bfc14ec10b0dc15968798d3
KJ <tongkj2000@gmail.com>
2022-01-25 14:38:20 +0800

    Fixed typo in Main

--- src/Main.java
+++ src/Main.java
@@ -13,9 +13,9 @@ public class Main {
         numberOfYears = input.nextInt();
         System.out.print("Enter loan amount, for example, 120000.95: ");
         loanAmount = input.nextDouble();
-        Loan myloan = new Loan(interestRate, numberOfYears, loanAmount);
+        Loan myLoan = new Loan(interestRate, numberOfYears, loanAmount);
         System.out.format("The loan was created on %s.\n", date);
-        System.out.format("The monthly payment is $%.2f.\n", myloan.getMonthlyPayment());
-        System.out.format("The total payment is $%.2f.\n", myloan.getTotalPayment());
+        System.out.format("The monthly payment is $%.2f.\n", myLoan.getMonthlyPayment());
+        System.out.format("The total payment is $%.2f.\n", myLoan.getTotalPayment());
     }
 }
